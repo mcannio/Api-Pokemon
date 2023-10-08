cb3d9d14e2d17f9028f501448d6ab11d927e83f6
Marta Caño <marta.cano88@gmail.com>
2023-10-08 13:38:03 +0200

    actualizacion 2

--- pokedex.js
+++ pokedex.js
@@ -1,35 +1,33 @@
-const pokedex = document.getElementById('pokedex');
-// fetch('https://pokeapi.co/api/v2/pokemon/')
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((myJson) => {
-//     console.log(myJson);		
-//   });
+const pokedex = document.getElementById('pokedex'); // COJO EL OBJETO POKEDEX DE HTML
 
-const getPokemon = async () => {
+const getPokemon = async () => { // RECOJO DATOS DE LA API Y LOS ALMACENO EN UN ARRAY
     let myArrayPokemon = [];
-    for (let index = 1; index <= 151; index++) {    
+    for (let index = 1; index <= 151; index++) {  // CREO UN BUCLE PARA RECOGER EN EL ARRAY LOS 151  
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${index}`);    
     const res = await response.json();
     // console.log(res);
-    myArrayPokemon.push(res);
+    myArrayPokemon.push(res); // AÑADO ELEMENTOS
     }
     // console.log(myArrayPokemon)
-    const pokemon = myArrayPokemon.map((result) => ({
+    const pokemon = myArrayPokemon.map((result) => ({ // CREO UNA CONSTANTE PARA MAPEAR LOS DATOS DE LA API
         name: result.name,
         image: result.sprites['front_default'],
         type: result.types.map((type) => type.type.name).join(', '),
-        id: result.id
+        id: result.id,
+        ability: result.abilities.map((ability) => ability.ability.name).join(', '),
     }));
+    myArrayPokemon.push(pokemon);
     // console.log(pokemon)
     return pokemon;
-}
-const listaPokemon = async () => {
-    const pokemonList = await getPokemon()
-    console.log(pokemonList);
+    
+}   
+const listaPokemon = async (searchTerm ="") => {  //¿?
+    const pokemonList = await getPokemon();
+    const filteredPokemonList = filtrarPokemon(pokemonList, searchTerm); // BUSCO POR NOMBRE
+    // console.log(pokemonList);
     const ol = document.getElementById('pokedex');
-    for (const pokemon of pokemonList) {
+    ol.innerHTML = ''; // INSERTO LA INFO EN HTML
+    for (const pokemon of filteredPokemonList) {
         const li = document.createElement('li');         
         const div = document.createElement('div');
         div.setAttribute("class","flip-card");
@@ -43,61 +41,39 @@ const listaPokemon = async () => {
         img.setAttribute("src", pokemon.image);
         img.setAttribute("alt", "Avatar"); 
         img.setAttribute("style", "width:200px;height:200px;"); 
-        const h1 = document.createElement("h1")
-        h1.innerText = pokemon.name;
-        const p = document.createElement("p")
-        p.innerText = pokemon.type;
+        const h1 = document.createElement("h1");        
+        h1.innerText = mayus (pokemon.name);       
+        const p = document.createElement("p");
+        p.innerText = `Type: ${pokemon.type}`;
+        const p1 = document.createElement("p");
+        p1.innerText =  `Abilities: ${pokemon.ability}`;
+        
         div4.appendChild(h1);
         div4.appendChild(p);     
-
+        div4.appendChild(p1);
 
         div3.appendChild(img); 
         div2.appendChild(div3);
         div2.appendChild(div4);
         div.appendChild(div2);
         li.appendChild(div);        
-        ol.appendChild(li);
-
-        const listaPokemon = (pokemonList) => {
-            const input$$ = document.querySelector("input");
-          
-            input$$.addEventListener("input", () =>
-              searchPokemons(input$$.value, pokemonList)
-            );
-          };
-          
-          const searchPokemons = (filtro, pokemons) => {
-            let pokemonsFiltrados = pokemons.filter((pokemon) =>
-              pokemon.name.toLowerCase().includes(filtro.toLowerCase())
-            );
-          
-            listaPokemon(pokemonsFiltrados);
-          };
-
-        
-        // const searchPokemon = async () => {
-        //     const searchTerm = document.getElementById('pokemonName').value.toLowerCase();
-        //     const filteredPokemon = await getPokemon();
-        //     const searchResults = filteredPokemon.filter(pokemon => pokemon.name.includes(searchTerm));
-        //     const ol = document.getElementById('pokedex');
-        //     ol.innerHTML = '';
-        //     for (const pokemon of searchResults) {
-                
-        //     }
-        
-        
-        // const input$$ = document.querySelector("input");
-        // let searchPokemons = (filtro, pokemons) =>{
-        //     console.log(filtro, pokemons);
-        // }
-        // input$$.addEventListener("click",()=>
-        // searchPokemons(input$$.value, pokemonList);
-        // console.log(searchPokemons(input$$, pokemonList));
+        ol.appendChild(li);             
         
     }
+    
+};
+const filtrarPokemon = (pokemonList, searchTerm) => {
+    return pokemonList.filter(pokemon => {
+        return pokemon.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+};
+function mayus(palabra) { // FUNCIÓN PARA PONER EN MAYÚSCULA LA PRIMERA LETRA DEL NOMBRE
+    return palabra.charAt(0).toUpperCase() + palabra.slice(1);
 }
-listaPokemon()
-
-// const init = async () => {
-
-// init();
+listaPokemon(); // LLAMO A LA FUNCIÓN PRINCIPAL
+const searchButton = document.getElementById('searchButton'); //BUSCADOR
+const searchInput = document.getElementById('searchInput');
+searchButton.addEventListener('click', () => {
+    const searchTerm = searchInput.value;
+    listaPokemon(searchTerm);
+});
